7a66134dc14e5958f5082c20bd2c25b6a7b5d523
rdhalwala <126441959+rdhalwala@users.noreply.github.com>
2023-11-09 00:42:58 +0000

    lab 6

--- answers.md
+++ answers.md
@@ -1,20 +1,32 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
+**Name:**_Raiya Dhalwaa_________________
 **Name:**_________________________
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
 
-
 - **2)**
+- W(n-1)+W(n-2)+C
+- W(0)=0
+- W(1)=1
+- 
 
 - **3)**
+- S(n-1)+S(n-2)+C
 
 - **4)**
+- The pattern for 10 is 34	55	34	21	13	8	5	3	2	1	1
+- This is odd because it goes from 34 up to 55 then down to 34 and then decreases until it hits 10. This is the recursion working backwards, but the beginning of the list is odd.
+
 
 - **6)**
+- The most times fib_top_down(i) will be called for any valu e i will be 2 becuase every number added to fibs is calculated once.
+- Max done in paralell is same as n fibonacci number computed.
+The work and span are: O(n)
 
 - **8)**
+The most will be n amount because it iterates through so goes one by one. 
+The work and span are O(1) because it is iterative
--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 def fib_recursive(n, counts):
-    """
+  """
     Return the nth Fibonacci number. 
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
-    """    
-    counts[n] += 1
-    ###TODO
-    pass
-    
+    """
+  counts[n] += 1
+  ###TODO
+  #base case
+  if n <= 1:
+    return n
+  else:
+    #Recursive
+    return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
 
-    
+
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+  ###TODO
+  if fibs[n] != -1:
+    return fibs[n]
+  elif n <= 1:
+    fibs[n] = n
+  else:
+    fibs[n] = fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
+  return fibs[n]
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
-
-
-
-
-def fib_bottom_up_better(n):
-    ###TODO
-    pass
+  ###TODO
+  ran = range(2, n + 1)
+  result = 0
+  x = 0
+  y = 1
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    for i in ran:
+      result = x + y
+      x = y
+      y = result
+    return result
